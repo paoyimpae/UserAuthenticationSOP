1304b9c888da5dc9198264f263f579b4a94b2ae2
CaptKen <60070121@kmitl.ac.th>
2019-10-30 18:46:35 +0700

    Create + try to return list

--- userManagement/src/main/java/usermanagement/sop/controllers/UserController.java
+++ userManagement/src/main/java/usermanagement/sop/controllers/UserController.java
@@ -1,6 +1,9 @@
 package usermanagement.sop.controllers;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +18,12 @@ public class UserController {
     @Autowired
     FirebaseService firebaseService;
     @PostMapping("/add")
-    public boolean addUser(@RequestBody User user) {
-        boolean data = firebaseService.addUser(user);
-        return data;
+    public void addUser(@RequestBody User user) throws ExecutionException, Exception {
+        firebaseService.addUser(user);
+    }
+    
+    @GetMapping("/list")
+    public void getAllUsers() throws Exception {
+    	firebaseService.getUsers();
     }
 }
--- userManagement/src/main/java/usermanagement/sop/services/FirebaseService.java
+++ userManagement/src/main/java/usermanagement/sop/services/FirebaseService.java
@@ -1,8 +1,13 @@
 package usermanagement.sop.services;
 
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -10,9 +15,12 @@ import org.springframework.stereotype.Service;
 import usermanagement.sop.models.User;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class FirebaseService {
@@ -22,13 +30,50 @@ public class FirebaseService {
         initDB();
     }
 
-    public boolean addUser(User user) {
-        String userId = UUID.randomUUID().toString();
-        CollectionReference colRef = db.collection("users");
-        Map<String, User> data = new HashMap<>();
-        data.put(userId, user);
-        colRef.add(data);
-        return true;
+    public void addUser(User user) throws Exception, ExecutionException {
+//        String userId = UUID.randomUUID().toString();
+//        CollectionReference colRef = db.collection("users");
+//        Map<String, User> data = new HashMap<>();
+//        data.put(userId, user);
+//        colRef.add(data);
+//        return true;
+    	
+    	DocumentReference docRef = db.collection("users").document();
+    	Map<String, Object> data = new HashMap<>();
+    	data.put("name", user.getName());
+    	data.put("password", user.getPassword());
+    	data.put("role", user.getRole());
+    	data.put("username", user.getUsername());
+    	//asynchronously write data
+    	ApiFuture<WriteResult> result = docRef.set(data);
+    	// ...
+    	// result.get() blocks on response
+    	System.out.println("Update time : " + result.get().getUpdateTime());
+    }
+
+
+    public List<User> getUsers() throws Exception {
+    	
+    	CollectionReference colRef = db.collection("users");
+    	colRef.get();
+        // [START fs_get_all]
+        // asynchronously retrieve all users
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+        // ...
+        // query.get() blocks on response
+        QuerySnapshot querySnapshot = query.get();
+        List<User> allUser = new ArrayList<>();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            System.out.println("User: " + document.getId());
+            System.out.println("Name: " + document.getString("name"));
+            System.out.println("Password: " + document.getString("password"));
+            System.out.println("Role: " + document.getString("role"));
+            System.out.println("Username: " + document.getString("username"));
+        }
+        return allUser;
+        
+//         [END fs_get_all]
     }
 
     private void initDB() throws IOException {
