7ed68aa99d29b425964b8ad12f588e0beac162ac
Tanakorn <tanakorn.panthaikul@gmail.com>
2019-11-18 22:59:03 +0700

    Sendding Email when Register

--- ohmProj/ohmProj/settings.py
+++ ohmProj/ohmProj/settings.py
@@ -70,6 +70,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ohmProj.wsgi.application'
 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ohmzaaaaaaa@gmail.com'
+EMAIL_HOST_PASSWORD = 'toshiro1'
+EMAIL_PORT = 587
+
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
