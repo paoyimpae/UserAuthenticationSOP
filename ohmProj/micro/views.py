cc0633f62cda75336ef966fcf19545a94a780d7a
CaptKen <60070121@kmitl.ac.th>
2019-11-21 01:16:48 +0700

    test

---
+++ ohmProj/micro/views.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
+from micro.forms import SignupForm
+from micro.tokens import account_activation_token
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('micro/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'micro/signup.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+def home(request):
+    return render(request, template_name='micro/home.html')
+
+
+def my_login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+
+            # next_url = request.POST.get('next_url')
+            # if next_url:
+            #     return redirect(next_url)
+            # else:
+            #     return redirect('index2')
+        else:
+            context['username'] = username
+            context['password'] = password
+            context['error'] = 'Wrong username or password'
+
+    # next_url = request.GET.get('next')
+    # if next_url:
+    #     context['next_url'] = next_url
+
+    return render(request, template_name='micro/login.html', context=context)
+
+
+def my_logout(request):
+    logout(request)
+    return redirect('login')
