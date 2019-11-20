e39e33e23f6eb3d230000eee433a411d32ec1e10
Tanakorn <tanakorn.panthaikul@gmail.com>
2019-11-21 00:22:57 +0700

    fix setting

--- ohmProj/ohmProj/settings.py
+++ ohmProj/ohmProj/settings.py
@@ -73,8 +73,8 @@ WSGI_APPLICATION = 'ohmProj.wsgi.application'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ohmzaaaaaaa@gmail.com'
-EMAIL_HOST_PASSWORD = 'toshiro1'
+EMAIL_HOST_USER = 'authen.service.app@gmail.com'
+EMAIL_HOST_PASSWORD = 'Qwertyui*'
 EMAIL_PORT = 587
 
 
