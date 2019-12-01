20c04a2578ae0c27a6ffb74f2396a50c4daa06d4
paoyimpae <paoyimpae@hotmail.com>
2019-12-02 02:27:15 +0700

    ✅ Edit Fix 👌

--- ohmProj/micro/serializers.py
+++ ohmProj/micro/serializers.py
@@ -5,14 +5,15 @@ from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        help_text='Leave empty if no change needed',
-        style={'input_type': 'password', 'placeholder': 'Password'}
-    )
+    ### in this case, password can't be saved in cypher word ! 
+    # password = serializers.CharField(
+    #     write_only=True,
+    #     required=True,
+    #     help_text='Leave empty if no change needed',
+    #     style={'input_type': 'password', 'placeholder': 'Password'}
+    # )
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'telephone', 'group')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'telephone', 'group')
+        # extra_kwargs = {'password': {'write_only': True}}
